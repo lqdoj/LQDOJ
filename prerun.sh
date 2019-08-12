f81aa1e11735df35887e8f5e1597949328b92033
winkhanh <giakhanhnk2000@gmail.com>
2019-08-12 08:38:52 +0700

    Fix prerun

--- prerun.sh
+++ prerun.sh
@@ -1,6 +1,6 @@
-cho "Setting up..."
+echo "Setting up..."
 cd back-end && pip3 install django-rest-framework && pip3 install django-cors-headers && python3 manage.py migrate 
 cd ..
-cd fron-tend && yarn install 
+cd front-end && yarn install 
 cd .. 
 echo "Completed setup"
